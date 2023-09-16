1147f6bbac9dd7acaaddc54b7f1c3409c7328dd7
Erick-Amorim <erick7amorim@gmail.com>
2023-09-16 15:34:17 -0300

    resolution of java stdin and stdout 1 problem

---
+++ Java/java-stdin-and-stdout-I/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondtNumber = scanner.nextInt();
+        int thirdNumber = scanner.nextInt();
+
+        System.out.println(firstNumber);
+        System.out.println(secondtNumber);
+        System.out.println(thirdNumber);
+    }
+}
